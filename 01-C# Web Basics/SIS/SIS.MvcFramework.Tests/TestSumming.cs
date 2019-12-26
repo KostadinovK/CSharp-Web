c272f85ae9002841e2b0eb7f34427319b5662b1d
KostadinovK <kostadinovk2001@gmail.com>
2019-12-26 12:37:07 +0200

    Added project for SIS.MvcFramework xUnit tests

---
+++ 01-C# Web Basics/SIS/SIS.MvcFramework.Tests/TestSumming.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace SIS.MvcFramework.Tests
+{
+    public class TestSumming
+    {
+       [Fact]
+       public void Sum2Plus2()
+       {
+           Assert.Equal(4, 2 + 2);
+       }
+    }
+}
